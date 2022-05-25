8e5bf851af3a7da2064d96855ceaeb41914daa89
IvanTtsenov <ivan.cenov95@abv.bg>
2022-05-25 22:32:38 +0300

    Create preferences.js

---
+++ Helper/dwaaw/preferences.js
@@ -0,0 +1,102 @@
+// {
+//     "type": "node",
+//     "request": "launch",
+//     "name": "Launch Program",
+//     "program": "${file}"
+// }
+// File > Preferences > Settings > Launch v tusachkata > edit settings > postavqm koda 
+
+// function solve(input) {
+
+// }
+// solve(["11","Monday","Monday","Monday","Monday"])
+
+// regex101 -ИЗБЕРИ ECMASCRIPT
+
+// function solve(a) {
+
+// }
+// solve()
+
+// npm init -y - package.json file
+// npm install mocha chai --save-dev
+// for playwright: npm install --save-dev playwright-chromium
+// package.json - test: "mocha"
+// remove ERR! messages - npm run test -s OR
+// package.json - test: "mocha || true" automatic
+// use .deep.equl when the answer is object or array!!!
+
+// const {expect} = require('chai');
+// const func = require('./1RequestValid')
+
+// -----------
+// const { chromium } = require('playwright-chromium');
+
+// (async () => {
+
+// const browser = await chromium.launch();//to see test add {headless:false,slowMo:2000}
+// const page = await browser.newPage();
+// await page.goto('https://google.com/');
+// await page.screenshot({ path: `example.png` });
+// await browser.close();
+// })();
+// ---------------------
+// F1 stop infinite loop
+// ctrl + markirane za komentar
+
+// alt + klik za dobavanq ili + backspace za kachvane
+// na 1 red
+
+
+// fetch(url).then(res => {
+// if (res.ok == false) {
+// throw new Error(`${res.status} ${res.statusText}`);
+// }
+// return res.json();
+// })
+// .then(handleResponse)
+// .catch(handleError);
+
+// --------------------------------------------------------
+
+// try {
+// const res = await fetch(url);
+// if (res.ok == false) {
+// throw new Error(`${res.status} ${res.statusText}`);
+// }
+// const data = await res.json();
+// handleResponse(data);
+// } catch (error) {
+// handleError(error);
+// }
+
+// import { html,render } from '../node_modules/lit-html/lit-html.js';
+// import {until} from '../node_modules/lit-html/directives/until.js';
+// import page from '../node_modules/page/page.mjs';
+
+
+// FONT AWESOME
+
+// <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css">
+
+// OVERLAY color
+
+// ::after {
+// content: '';
+// position:absolute;
+// z-index: Some number;
+// top,left,right,bottom: 0;
+// background-color: color;
+// opacity: btw 0 and 1
+
+// TYPESCRIPT compilator
+// tsc main.ts --target es6
+
+// CREATE A NEW COMPONENT IN ANGULAR
+// ng g c name
+// ng g(short for generate ) c(short for component) name(whatever name of the files)
+
+// CREATE NEW SERVICES IN ANGULAR
+// ng g s name
+
+// ng g(short for generate ) s(short for service) name(whatever name of the files)
